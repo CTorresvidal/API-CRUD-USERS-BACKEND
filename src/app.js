2704e5a87f2cef32354a40159b8debcf1c2d4ab3
Carlos <carlostorres2912@yahoo.com.com>
2024-02-26 18:34:45 -0500

    first-commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const router = require('./routes');
+const errorHandler = require('./utils/errorHandler');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+}));
+app.use(cors());
+
+app.use(router);
+app.get('/', (req, res) => {
+    return res.send("Servidor corriendo OK!");
+})
+
+app.use(errorHandler)
+
+module.exports = app;
